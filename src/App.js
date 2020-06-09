7ecd3200e96718eca433bbb73a14572542a95a22
Chris Stolba <chrisstolba@Chriss-MacBook-Pro.local>
2020-06-09 00:51:11 -0700

    Finished the HTML structure.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import React from "react";
 import "./css/reset.css";
 import "./css/main.css";
 
+import Header from "./components/Header.js";
+import Card from "./components/Card.js";
+
+import cardInfo from "./data/cardInfo.js";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        {cardInfo.map((card, index) => (
+          <Card
+            key={index}
+            title={card.title}
+            description={card.description}
+            icon={card.icon}
+          />
+        ))}
+      </main>
+    </div>
+  );
 }
 
 export default App;
